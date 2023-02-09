d3bb17a50a35f72bd77b5e3a08b3df48ae94fd88
PeterNguyenWebDev <peternguyenwebdev@gmail.com>
2023-02-09 14:07:10 -0800

    Syntax Update

--- page-staff.php
+++ page-staff.php
@@ -20,11 +20,15 @@ get_header();
 		<?php while ( have_posts() ) : the_post(); ?>
             
 		<header class="entry-header">
-			<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
+            <h1 class="entry-title">
+			<?php esc_html_e(get_the_title()); ?>
+            </h1>
 		</header>
 
         <section class="entry-content">
-        <?php the_content() ?>
+            
+            <?php the_content(); ?>
+            
         </section>
 
         <?php
@@ -53,33 +57,42 @@ get_header();
                 $query = new WP_Query( $args );
                     
                 if ( $query -> have_posts() ) {
-                    echo '<section class="staff-term-wrapper">';
-                    echo '<h2>' . esc_html( $term->name ) . '</h2>';
-
+                    ?>
+                    <section class="staff-term-wrapper">
+                    <h2> <?php esc_html_e( $term->name ) ?> </h2>
+                    <?php
                     while ( $query -> have_posts() ) {
                         $query -> the_post();
                     
                         if ( function_exists( 'get_field' ) ) {
-                            echo '<article class="staff-article">';
+                            ?>
+                            <article class="staff-article">
+                            <?php
                             if ( get_field( 'staff_biography' ) ) {
-                                echo '<h3>' . esc_html( get_the_title()) . '</h3>';
-                                the_field( 'staff_biography' );
+                                ?>
+                                <h3> <?php esc_html_e( get_the_title()) ?></h3>
+                                <p> <?php esc_html_e(get_field( 'staff_biography' )); ?></p>
+                            <?php
                             }
                             if ( get_field( 'staff_courses' ) ) {
-                                echo '<p> Courses: '; 
-                                the_field( 'staff_courses' );
-                                echo '</p>';
+                                ?>
+                                <p>Courses: <?php esc_html_e(get_field( 'staff_courses' )); ?> </p>
+                                <?php
                             }
                             if ( get_field( 'staff_website' ) ) {
-                                echo '<a href="';
-                                esc_url(the_field( 'staff_website' ));
-                                echo '">Instructor Website</a>';
+                                ?>
+                                <a href="<?php echo esc_url(get_field( 'staff_website' )); ?>">Instructor Website</a>
+                            <?php
                             }
-                            echo '</article>';
+                            ?>
+                            </article>
+                        <?php
                         }
                     }
                     wp_reset_postdata();
-                    echo '</section>';
+                    ?>
+                    </section>
+                    <?php
                 }
             }
         }

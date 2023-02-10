4a342a998103fa6404db7fd2b6e27b9f96b27d97
PeterNguyenWebDev <peternguyenwebdev@gmail.com>
2023-02-10 01:13:23 -0800

    Add Menu SVG, add header & footer styles

--- header.php
+++ header.php
@@ -46,7 +46,13 @@
 		</div><!-- .site-branding -->
 
 		<nav id="site-navigation" class="main-navigation">
-			<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Primary Menu', 'fwd-school' ); ?></button>
+			<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false">
+				<span class="screen-reader-text">Menu</span>
+				<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
+					<path d="M4 22h-4v-4h4v4zm0-12h-4v4h4v-4zm0-8h-4v4h4v-4zm3 0v4h17v-4h-17zm0 12h17v-4h-17v4zm0 8h17v-4h-17v4z"/>
+				</svg>
+			</button>
+			
 			<?php
 			wp_nav_menu(
 				array(

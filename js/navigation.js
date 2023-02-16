a378d4d7c1ad96ae54f3faaf0f057fecb95cfda8
PeterNguyenWebDev <peternguyenwebdev@gmail.com>
2023-02-15 19:21:13 -0800

    Nav JS cleanup

--- js/navigation.js
+++ js/navigation.js
@@ -103,16 +103,12 @@
 	const mediaQuery = window.matchMedia('(min-width: 40rem)')
 
 	function handleScreenChange(e) {
-	// Check if the media query is true
-	if (e.matches) {
-		// Then log the following message to the console
-		console.log('Media Query Matched!');
-		siteNavigation.classList.remove( 'toggled' );
-		button.setAttribute( 'aria-expanded', 'false' );
-	};
+		if (e.matches) {
+			siteNavigation.classList.remove( 'toggled' );
+			button.setAttribute( 'aria-expanded', 'false' );
+		};
 	};
 
-	// Register event listener
 	mediaQuery.addEventListener('change', handleScreenChange);
 
 }() );
